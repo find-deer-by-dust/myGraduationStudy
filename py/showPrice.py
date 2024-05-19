bb42a10fde9ca418693b8b7fa572148550a34fea
findDeerByDust <1915141975@qq.com>
2024-05-19 23:44:27 +0800

    初稿终于写完了.累死了

--- py/showPrice.py
+++ py/showPrice.py
@@ -15,20 +15,22 @@ df.index.names = ['time']
 
 start='2016-01-01'
 end='2017-01-01'
+start='2018-01-01'
+end='2019-06-01'
 index_pos1 = df.index.get_loc(start)
 index_pos2 = df.index.get_loc(end)
 
 data=df.iloc[index_pos1:index_pos2]
 
 import matplotlib.pyplot as plt
-
+plt.rcParams['font.family'] = 'SimHei'  # 替换为你选择的字体
 # 绘制折线图
 plt.plot(data.index, data['price'])
 
 # 添加标题和标签
-plt.title('Price Trend from '+start+' to '+end)
+plt.title('黄金价格从 '+start+' 到 '+end)
 plt.xlabel('Date')
-plt.ylabel('Price')
+plt.ylabel('黄金价格')
 
 # 显示网格
 plt.grid(True)
